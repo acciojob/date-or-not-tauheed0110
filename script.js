645a967bfa2b34bc44712109054e4c7904edda9a
tauheed0110 <tauheed0110@gmail.com>
2024-11-05 15:27:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 var isDate = function (input) {
   //   write your code here
-  if(input instanceof Date){
-    return !isNaN(input.getTime())
-  }
-  if(typeof input === 'string'){
-    const parsedDate = new Date(input);
-    return !isNaN(parsedDate.getTime());
-  }
+  return !isNaN(Date.parse(input));
 };
 
 // Do not change the code below.
